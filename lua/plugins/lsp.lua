0046dc8fcb1602f4e59b474cc96a4a0408ae12a4
eddyli <eddyli@outlook.com>
2026-07-12 14:23:48 +0800

    fix: correct nvim-cmp ConfirmBehavior casing from lower to PascalCase

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -76,7 +76,7 @@ return {
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<CR>"] = cmp.mapping.confirm({
-                        behavior = cmp.confirm_behavior.Replace,
+                        behavior = cmp.ConfirmBehavior.Replace,
                         select = true,
                     }),
                     ["<Tab>"] = cmp.mapping(function(fallback)
