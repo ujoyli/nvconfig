dcb0b3f1c228a7f88fcadc56f2220ce30716a91b
eddyli <eddyli@outlook.com>
2026-07-12 14:53:47 +0800

    feat: switch default colorscheme to dracula

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,23 +6,12 @@
 
 return {
     {
-        "folke/tokyonight.nvim",
+        "Mofiqul/dracula.nvim",
         lazy = false,
         priority = 1000, -- Load this colorscheme first before other plugins
         config = function()
-            require("tokyonight").setup({
-                style = "moon", -- storm, night, moon, day
-                transparent = false,
-                terminal_colors = true,
-                styles = {
-                    comments = { italic = true },
-                    keywords = { italic = true },
-                    functions = {},
-                    variables = {},
-                },
-            })
             -- Enable the colorscheme
-            vim.cmd([[colorscheme tokyonight-moon]])
+            vim.cmd([[colorscheme dracula]])
         end,
     },
 }
