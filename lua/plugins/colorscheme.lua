b544ddb09319efbb5363bf3e2d544908fced0067
eddyli <eddyli@outlook.com>
2026-07-12 14:56:03 +0800

    feat: customize dracula palette to match Dracula Remastered (Dracula 2) colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,6 +10,15 @@ return {
         lazy = false,
         priority = 1000, -- Load this colorscheme first before other plugins
         config = function()
+            local dracula = require("dracula")
+            dracula.setup({
+                colors = {
+                    bg = "#1E1F35",      -- Dracula Remastered background (deep ocean navy)
+                    green = "#21E4B3",   -- Turquoise for functions
+                    orange = "#FF9E95",  -- Coral for parameters/macros
+                    purple = "#c299ff",  -- Bright lavender for constants/readonly variables
+                },
+            })
             -- Enable the colorscheme
             vim.cmd([[colorscheme dracula]])
         end,
